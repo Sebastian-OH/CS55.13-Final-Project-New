3ce202019a5b0cabdd6d157814ed367dc738b69f
Sebastian H <SebastianHelguera@rafaels-imac.attlocal.net>
2023-12-09 11:34:16 -0800

    back to normal

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -109,26 +109,19 @@ export interface UserPhoto {
   webviewPath?: string;
 }
 
-export async function base64FromPath(path: string): Promise<string | Blob> {
+export async function base64FromPath(path: string): Promise<string> {
   const response = await fetch(path);
   const blob = await response.blob();
-
-  return new Promise<string | Blob>((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    
     reader.onerror = reject;
-
     reader.onload = () => {
       if (typeof reader.result === 'string') {
         resolve(reader.result);
-      } else if (reader.result instanceof ArrayBuffer) {
-        // If it's ArrayBuffer, cast it to string
-        resolve(String.fromCharCode.apply(null, new Uint16Array(reader.result as ArrayBuffer)));
       } else {
-        reject('Method did not return a string');
+        reject('method did not return a string')
       }
     };
-
     reader.readAsDataURL(blob);
   });
 }
